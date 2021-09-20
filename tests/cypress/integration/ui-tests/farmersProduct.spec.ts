e79cf52e2b19c7e3d7d1294dad802ccc4ca5d10f
Cynthia <cynthia.tan@weareroam.com>
2021-09-20 16:33:11 +1200

    feat: added first merchant test page

---
+++ tests/cypress/integration/ui-tests/farmersProduct.spec.ts
@@ -0,0 +1,17 @@
+import { merchantProductPage } from "../../page-objects/merchantProductPage";
+
+describe('Farmers Product Page Injection Test', () => {
+    beforeEach(() => {
+        merchantProductPage.visitAndInjectWidget(
+            'http://www.farmers.co.nz/fisher-paykel-614l-french-door-fridge-freezer-stainless-steel-rf610anux5-6604672',
+            '[class="mobile-check-store-container"]',
+            ''
+        )
+    })
+
+    it('The Widget should be visible after injection', () => {
+        cy.get('#humm-widgets-cards-nz').should('be.visible')
+    })
+})
+
+
---
+++ tests/cypress/page-objects/merchantProductPage.ts
@@ -0,0 +1,49 @@
+class _merchantProductPage {
+
+    produdctPrice: string = '300'
+
+    visitAndInjectWidget(url: string, targetElement: string, position: string) {
+        cy.visit(url, {
+            onLoad: contentWindow => {
+                return new Cypress.Promise((resolve, reject) => {
+                    // create script element
+                    let tag = document.createElement('script')
+
+                    // append script element
+                    let el = cy.$$(<any>contentWindow.document.body).find(targetElement)
+
+                    switch (position) {
+                        case 'append':
+                            el.append(tag)
+                            break
+                        case 'prepend':
+                            el.prepend(tag)
+                            break
+                        case 'after':
+                            el.after(tag)
+                            break
+                        default:
+                            el.before(tag)
+                            break
+                    }
+
+                    // Add an onload event to script tag to resolve promise
+                    tag.onload = () => {
+                        resolve()
+                    }
+
+                    // set source of element (Note, must be done post injection, else the script wont have a "DOM" location to inject
+                    tag.src =
+                        'http://localhost:5000/humm-widgets-cards-nz.umd.js?productPrice=300'
+
+                    // Add a timeout, to reject promise after 2.5s of not "load" fired
+                    setTimeout(() => {
+                        tag.onload = null
+                        reject()
+                    }, 3000)
+                })
+            },
+        })
+    }
+}
+export const merchantProductPage = new _merchantProductPage();
