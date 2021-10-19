b09f2e8cda71bb7b395808e915563cd534d4031a
arjun-at-roam <63985172+arjun-at-roam@users.noreply.github.com>
2021-10-19 13:01:34 +1100

    feat: update product language only for required overlay breakdown

--- src/lang/ResponseLanguage.ts
+++ src/lang/ResponseLanguage.ts
@@ -1,22 +1,14 @@
 export const ProductLanguage = [
-  { name: 'id', label: 'id' },
-  { name: 'type', label: 'Type' },
-  { name: 'interestFreePeriod', label: 'Interest free period' },
-  { name: 'termPeriod', label: 'Term period' },
-  { name: 'termRate', label: 'Term rate' },
-  { name: 'deferredPeriod', label: 'Deferred period' },
-  { name: 'establishmentFee', label: 'Establishment fee' },
-  { name: 'description', label: 'Description' },
+  { name: 'interestFreePeriod', label: 'Interest free period', unit: 'months' },
+  { name: 'establishmentFee', label: 'Establishment fee', unit: '$' },
   {
-    name: 'interestFreeMonthlyRepayment',
+    name: 'indicativeMinMonthly',
+    label: 'Indicative minimum monthly repayments*',
+    unit: '$',
+  },
+  {
+    name: 'indicativeMonthly',
     label: 'Indicative repayment to pay before Interest free period expires ^',
-  },
-  {
-    name: 'minTermMonthlyRepayment',
-    label: 'Minimum term monthly repayment',
-  },
-  {
-    name: 'remainderInterestPeriodMontlyRepayment',
-    label: 'Reminder interest period monthly repayment',
+    unit: '$',
   },
 ]
