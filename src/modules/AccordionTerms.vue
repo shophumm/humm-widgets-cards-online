10d065d7d69ba1410d3aaf8adf94e24e9338bf7a
arjun-at-roam <63985172+arjun-at-roam@users.noreply.github.com>
2021-11-16 10:24:33 +1100

    feat: create new module AccordionTerms

---
+++ src/modules/AccordionTerms.vue
@@ -0,0 +1,42 @@
+<template>
+  <Accordion :id="id" :title="title" :content="termsTemplate" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Accordion from 'src/components/accordion/Accordion.vue'
+// import Handlebars from 'handlebars'
+
+export default defineComponent({
+  name: 'AccordionTerms',
+  components: {
+    Accordion,
+  },
+  props: {
+    id: {
+      type: String,
+      default: 'widget-terms',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    termsTemplate: {
+      type: String,
+      required: true,
+    },
+    termsValues: {
+      type: Object,
+      required: true,
+    },
+  },
+  created() {
+    console.log(this.termsValues)
+    console.log(this.termsTemplate)
+    // const template = Handlebars.compile(this.termsTemplate)
+    // console.log(template(this.productData))
+  },
+})
+</script>
+
+<style scoped lang="scss"></style>
